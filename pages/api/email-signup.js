1e1addc1089900400fdcaee017d171df09a1e7ed
Quinn Keast <quinn@hey.com>
2022-02-05 20:13:39 +0100

    Fix email signups

--- pages/api/email-signup.js
+++ pages/api/email-signup.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: process.env.BUTTONDOWN,
+        Authorization: `Token ${process.env.BUTTONDOWN}`,
       },
       body: JSON.stringify(data),
     };
